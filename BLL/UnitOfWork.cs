3d61675969cd988dd78f4f85fa2da4464c04b1bf
EMRE KARAMEMİŞOĞLU <e.k.wissen19@gmail.com>
2019-05-29 12:41:13 +0300

    Add project files.

---
+++ BLL/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ApplicationDbContext _db = new ApplicationDbContext();
+        public UnitOfWork()
+        {
+        }
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
